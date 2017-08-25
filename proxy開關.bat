8e5affcecd5013973ce00aa9dead7b913ba983a9
spboxer3 <gdps82@yahoo.com.tw>
2017-08-25 20:08:40 +0800

    proxy switcher

---
+++ proxy開關.bat
@@ -0,0 +1,1 @@
+reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable|FINDSTR "1"&&reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyEnable" /t REG_DWORD /d 0 /f||reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyEnable" /t REG_DWORD /d 1 /f
